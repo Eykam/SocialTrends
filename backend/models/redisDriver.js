2978715cd94628c2ddf43df9e66b4ec32fce944d
Eyad <e.kam12@yahoo.com>
2024-05-18 20:29:02 -0400

    hardcoded

--- backend/models/redisDriver.js
+++ backend/models/redisDriver.js
@@ -6,7 +6,7 @@ let client;
 
 (async () => {
   client = redis.createClient({
-    url: process.env.REDIS_URL,
+    url: "redis://default:Thc!6154@social-trends-redis:6379",
   });
 
   client.on("error", (err) => console.log(err));
